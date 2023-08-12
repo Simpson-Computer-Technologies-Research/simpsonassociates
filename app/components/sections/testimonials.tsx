55e78aa6dab9acd530f5f31770c8aa03ae7a6a41
Tristan <heytristaann@gmail.com>
2023-08-12 08:34:27 -0400

    cleaned up testimonials. added href to each

--- app/components/sections/testimonials.tsx
+++ app/components/sections/testimonials.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
+import Link from "next/link";
 
 // Return the component
 export default function Testimonials(): JSX.Element {
@@ -77,7 +77,7 @@ const TestimonialCards = (props: { testimonials: any }): JSX.Element => {
  * @returns Testimonial Card Component JSX
  */
 const TestimonialCard = (props: { testimonial: any }) => (
-  <div className="mx-1 flex w-80 flex-col items-center p-4 text-center duration-500 ease-in-out hover:duration-0 xs:mx-3 md:mx-5 md:w-96 lg:w-[30rem]">
+  <div className="mx-1 flex w-80 flex-col items-center p-4 text-center duration-500 ease-in-out hover:bg-slate-50 hover:duration-100 xs:mx-3 xs:p-10 md:mx-5 md:w-96 lg:w-[30rem]">
     {" "}
     {/* hover:border-secondary */}
     <h2 className="mt-5 whitespace-nowrap text-base font-bold text-primary lg:text-lg">
@@ -90,10 +90,17 @@ const TestimonialCard = (props: { testimonial: any }) => (
       <StarSVG />
       <StarSVG />
     </div>
-    <p className="mt-5 text-xs font-medium text-primary sm:text-sm lg:text-base">
+    <p className="mb-7 mt-5 text-xs font-medium text-primary sm:text-sm lg:text-base">
       {props.testimonial.testimonial}
     </p>
-    <span className="mx-10 mb-6 mt-5 block h-1 w-2/5 rounded-full bg-secondary xs:w-1/4 sm:mb-10 sm:mt-7 lg:w-40"></span>
+    <Link
+      target="_blank"
+      rel="noopener noreferrer"
+      href={props.testimonial.href}
+      className="absolute bottom-6 w-40 bg-primary p-2 text-center text-sm text-white duration-500 ease-in-out hover:animate-pulse hover:brightness-110 sm:w-60 sm:text-base"
+    >
+      See full review
+    </Link>
   </div>
 );
 
@@ -123,78 +130,78 @@ const getTestimonials = (): any[] => [
   {
     name: "Louise Macdonald",
     testimonial:
-      "I have worked with Marita Talbot many times and she is a fabulous Mortgage agent. She is amazing with her clients and really works hard to put the deal together with the best possible rates. She's definitely the one to call in the Kitchener, Cambridge, Waterloo area and beyond!!",
-    image: "",
+      "I have worked with Marita Talbot many times and she is a fabulous Mortgage agent. She is amazing with her clients and really works hard to put the deal together with the best possible rates.",
+    href: "https://g.co/kgs/VDaJQU",
     key: "d3y6vuhjn",
   },
   {
     name: "M Margaret",
     testimonial:
-      "Fil from Dominion and Lending has helped make buying our first home as easy and stress free as possible. She explained everything clearly and diligently, and she also pays close attention to detail. I’ve never come across someone with as incredible communication, knowledge, and patience, the way that Fil has so kindly shown us.",
-    image: "",
+      "Fil from Dominion and Lending has helped make buying our first home as easy and stress free as possible. She explained everything clearly and diligently, and she also pays close attention to detail.",
+    href: "https://g.co/kgs/GWespF",
     key: "emdlf8jeu",
   },
   {
     name: "Elizabeth Chen",
     testimonial:
-      "We just bought our first home with the help of Dan and his team for the mortgage. It was a great experience working with them. Dan is very knowledgeable and knows what he does. The team does a great job answering our questions and providing lots of information.",
-    image: "",
+      "We just bought our first home with the help of Dan and his team for the mortgage. It was a great experience working with them. Dan is very knowledgeable and knows what he does.",
+    href: "https://g.co/kgs/yuZGvh",
     key: "fj3k4j3k4",
   },
   {
     name: "Jilu James",
     testimonial:
       "I would like to thank Paula for all her help to get best interest rate with a reputed bank. I highly recommend Dan and Paula for all your mortgage needs.",
-    image: "",
+    href: "https://g.co/kgs/a36Cf9",
     key: "hdiue83in",
   },
   {
     name: "Kelly Affeldt",
     testimonial:
       "Dan Simpson’s service is second to none! I have been sending my clients to Dan for their mortgage needs for over 10 years. Dan always has the time to assist his clients even on weekends, late evenings and even while he has been vacationing out of country!",
-    image: "",
+    href: "https://g.co/kgs/cs18se",
     key: "lpdkoeu3f",
   },
   {
     name: "Madison Lee",
     testimonial:
       "Dan Simpson and his team preform at an exceptional level. They work around the clock to provide the best service in the region. I highly recommend this team for a seamless mortgage experience.",
-    image: "",
+    href: "https://g.co/kgs/Laku65",
     key: "uidp9d7zg",
   },
   {
     name: "Keneisha Boreland",
     testimonial:
       "Thank you Richard for your great service. You were very patient and informative throughout the entire process. I appreciate your knowledge and the time you took to ensure we have the right  mortgage",
-    image: "",
+    href: "https://g.co/kgs/sK1KAf",
     key: "xlak267dj",
   },
   {
     name: "A&S Prestige Auto Sales",
     testimonial:
       "Richard was very professional and efficient to find the mortgages for us without any inconvenience, especially with the stress that the banks have at this moment for approving a mortgage. Thank you very much.",
-    image: "",
+    href: "https://g.co/kgs/mWVRuv",
     key: "lp04ahgdw",
   },
   {
     name: "Krystel Edwards",
     testimonial:
-      "I couldn’t imagine a better team to work with. The speed, care, and dedication to their clients is unmatched. I refer all my friends and family here, and I know they are getting the absolute best service!! Thank you all for being you!",
-    image: "",
+      "I couldn’t imagine a better team to work with. The speed, care, and dedication to their clients is unmatched. I refer all my friends and family here, and I know they are getting the absolute best service!!",
+    href: "https://g.co/kgs/4ADCax",
     key: "qqrta18ji",
   },
   {
-    name: "Karyn Zimmermann",
+    name: "Collin Beaumont",
     testimonial:
-      "Aaron de Souza was a pleasure to work with. He was very kind, knowledgeable, and professional. He worked extremely hard to get us a better rate than what our current bank was offering. Although we decided to remain where we are, we will stay in touch and hopefully be able to make the switch when our current term is up.",
-    image: "",
+      "The Dan Simpson Mortgage team was recommended to me by a realtor I have used for many years. His team and process was great and we could not be happier. ",
+    href: "https://g.co/kgs/ZGqJ1A",
     key: "lp0z0ghju",
   },
   {
     name: "Priscila Lopes",
     testimonial:
-      "We are so happy we chose Paula to help us with our mortgage! The whole process was super smooth; she understood our scenario and our expectations and helped us with the information we needed to buy our first home! She was always available and remembered every detail previously discussed, making our discussions consistently productive.",
-    image: "",
+      "We are so happy we chose Paula to help us with our mortgage! The whole process was super smooth; she understood our scenario and our expectations and helped us with the information we needed to buy our first home!",
+    href: "https://g.co/kgs/4rJdYv",
     key: "09jjkfise",
   },
 ];
--- pages/api/testimonials.ts
+++ pages/api/testimonials.ts
@@ -3,78 +3,78 @@ export default function handler(req: any, res: any) {
     {
       name: "Louise Macdonald",
       testimonial:
-        "I have worked with Marita Talbot many times and she is a fabulous Mortgage agent. She is amazing with her clients and really works hard to put the deal together with the best possible rates. She's definitely the one to call in the Kitchener, Cambridge, Waterloo area and beyond!!",
-      image: "",
+        "I have worked with Marita Talbot many times and she is a fabulous Mortgage agent. She is amazing with her clients and really works hard to put the deal together with the best possible rates.",
+      href: "https://g.co/kgs/VDaJQU",
       key: "d3y6vuhjn",
     },
     {
       name: "M Margaret",
       testimonial:
-        "Fil from Dominion and Lending has helped make buying our first home as easy and stress free as possible. She explained everything clearly and diligently, and she also pays close attention to detail. I’ve never come across someone with as incredible communication, knowledge, and patience, the way that Fil has so kindly shown us.",
-      image: "",
+        "Fil from Dominion and Lending has helped make buying our first home as easy and stress free as possible. She explained everything clearly and diligently, and she also pays close attention to detail.",
+      href: "https://g.co/kgs/GWespF",
       key: "emdlf8jeu",
     },
     {
       name: "Elizabeth Chen",
       testimonial:
-        "We just bought our first home with the help of Dan and his team for the mortgage. It was a great experience working with them. Dan is very knowledgeable and knows what he does. The team does a great job answering our questions and providing lots of information.",
-      image: "",
+        "We just bought our first home with the help of Dan and his team for the mortgage. It was a great experience working with them. Dan is very knowledgeable and knows what he does.",
+      href: "https://g.co/kgs/yuZGvh",
       key: "fj3k4j3k4",
     },
     {
       name: "Jilu James",
       testimonial:
         "I would like to thank Paula for all her help to get best interest rate with a reputed bank. I highly recommend Dan and Paula for all your mortgage needs.",
-      image: "",
+      href: "https://g.co/kgs/a36Cf9",
       key: "hdiue83in",
     },
     {
       name: "Kelly Affeldt",
       testimonial:
         "Dan Simpson’s service is second to none! I have been sending my clients to Dan for their mortgage needs for over 10 years. Dan always has the time to assist his clients even on weekends, late evenings and even while he has been vacationing out of country!",
-      image: "",
+      href: "https://g.co/kgs/cs18se",
       key: "lpdkoeu3f",
     },
     {
       name: "Madison Lee",
       testimonial:
         "Dan Simpson and his team preform at an exceptional level. They work around the clock to provide the best service in the region. I highly recommend this team for a seamless mortgage experience.",
-      image: "",
+      href: "https://g.co/kgs/Laku65",
       key: "uidp9d7zg",
     },
     {
       name: "Keneisha Boreland",
       testimonial:
         "Thank you Richard for your great service. You were very patient and informative throughout the entire process. I appreciate your knowledge and the time you took to ensure we have the right  mortgage",
-      image: "",
+      href: "https://g.co/kgs/sK1KAf",
       key: "xlak267dj",
     },
     {
       name: "A&S Prestige Auto Sales",
       testimonial:
         "Richard was very professional and efficient to find the mortgages for us without any inconvenience, especially with the stress that the banks have at this moment for approving a mortgage. Thank you very much.",
-      image: "",
+      href: "https://g.co/kgs/mWVRuv",
       key: "lp04ahgdw",
     },
     {
       name: "Krystel Edwards",
       testimonial:
-        "I couldn’t imagine a better team to work with. The speed, care, and dedication to their clients is unmatched. I refer all my friends and family here, and I know they are getting the absolute best service!! Thank you all for being you!",
-      image: "",
+        "I couldn’t imagine a better team to work with. The speed, care, and dedication to their clients is unmatched. I refer all my friends and family here, and I know they are getting the absolute best service!!",
+      href: "https://g.co/kgs/4ADCax",
       key: "qqrta18ji",
     },
     {
-      name: "Karyn Zimmermann",
+      name: "Collin Beaumont",
       testimonial:
-        "Aaron de Souza was a pleasure to work with. He was very kind, knowledgeable, and professional. He worked extremely hard to get us a better rate than what our current bank was offering. Although we decided to remain where we are, we will stay in touch and hopefully be able to make the switch when our current term is up.",
-      image: "",
+        "The Dan Simpson Mortgage team was recommended to me by a realtor I have used for many years. His team and process was great and we could not be happier. ",
+      href: "https://g.co/kgs/ZGqJ1A",
       key: "lp0z0ghju",
     },
     {
       name: "Priscila Lopes",
       testimonial:
-        "We are so happy we chose Paula to help us with our mortgage! The whole process was super smooth; she understood our scenario and our expectations and helped us with the information we needed to buy our first home! She was always available and remembered every detail previously discussed, making our discussions consistently productive.",
-      image: "",
+        "We are so happy we chose Paula to help us with our mortgage! The whole process was super smooth; she understood our scenario and our expectations and helped us with the information we needed to buy our first home!",
+      href: "https://g.co/kgs/4rJdYv",
       key: "09jjkfise",
     },
   ]);
