66861000047bc4de15b16655e9b0bc72b0abc735
Tristan <heytristaann@gmail.com>
2023-08-21 19:41:32 -0400

    tmp

--- app/lib/gcp.ts
+++ app/lib/gcp.ts
@@ -5,7 +5,7 @@ const storage: Storage = new Storage({
   projectId: process.env.GCP_PROJECT_ID,
   credentials: {
     client_email: process.env.GCP_CLIENT_EMAIL,
-    private_key: process.env.GCP_PRIVATE_KEY,
+    private_key: (process.env.GCP_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
     client_id: process.env.GCP_CLIENT_ID,
   },
 });
